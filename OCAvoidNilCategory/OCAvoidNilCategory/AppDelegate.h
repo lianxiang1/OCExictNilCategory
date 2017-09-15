44143f00819454b79476abec1db680067196ff5c
lianxiang1 <237807878@qq.com>
2017-09-16 00:00:39 +0800

    add

---
+++ OCAvoidNilCategory/OCAvoidNilCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCAvoidNilCategory
+//
+//  Created by lianxiang on 2017/9/15.
+//  Copyright © 2017年 lianxiangpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCAvoidNilCategory/OCAvoidNilCategoryTests/OCAvoidNilCategoryTests.m
@@ -0,0 +1,39 @@
+//
+//  OCAvoidNilCategoryTests.m
+//  OCAvoidNilCategoryTests
+//
+//  Created by lianxiang on 2017/9/15.
+//  Copyright © 2017年 lianxiangpro. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCAvoidNilCategoryTests : XCTestCase
+
+@end
+
+@implementation OCAvoidNilCategoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
